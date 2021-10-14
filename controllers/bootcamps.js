0aac10af7ed9f9da1bce9bd0941e37dcc9695725
Mithunmick <fazle.mithun@northsouth.edu>
2021-10-15 03:31:34 +0600

    Advanced Result Middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const ErrorResponse = require('../utili/errorResponse');
 const asyncHandler = require('../midleware/async');
 const Bootcamp = require('../models/Bootcamp');
@@ -8,74 +9,7 @@ const { parse } = require('dotenv');
 // @route  GET/api/v1/bootcamps
 //@access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  let query;
-  // Copy req.query
-  const reqQuery = { ...req.query };
-
-  //Firlds to exclude
-  const removeFields = ['select', 'sort', 'page', 'limit'];
-
-  // Loop over removeFields and delete them from reqQuery
-  removeFields.forEach(param => delete reqQuery[param]);
-
-  // Create query string
-  let queryStr = JSON.stringify(req.query);
-
-  // Create operstors ($gt, $gte, etc)
-  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-
-  // Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
-
-  // Select fields
-  if (req.query.select) {
-    const fields = req.query.select.split(',').join(' '); // using split methord to make array so if there is a coma make it in to array
-    query = query.select(fields);
-  }
-
-  // Sort
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort('-createAt');
-  }
-
-  // Pageination
-  const page = parseInt(req.query.page, 10) || 1; //paresInt is a js function to turn string to number
-  const limit = parseInt(req.query.limit, 10) || 25;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await Bootcamp.countDocuments();
-
-  query = query.skip(startIndex).limit(limit);
-
-  //Executing query
-  const bootcamps = await query;
-
-  //Pagination result
-  const Pageination = {};
-
-  if (endIndex < total) {
-    Pageination.next = {
-      page: page + 1,
-      limit,
-    };
-  }
-
-  if (startIndex > 0) {
-    Pageination.prev = {
-      page: page - 1,
-      limit,
-    };
-  }
-
-  res.status(200).json({
-    success: true,
-    count: bootcamps.length,
-    Pageination,
-    data: bootcamps,
-  });
+  res.status(200).json(res.advancedResults);
 
   // res
   //   .status(200)
@@ -180,4 +114,56 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
     count: bootcamps.length,
     data: bootcamps,
   });
+});
+
+// @desc   Upload photo for bootcamp
+// @route  PUT/api/v1/bootcamps/:id/photo
+//@access  Private
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  if (!req.files) {
+    return next(new ErrorResponse(`Please upload a file`, 400));
+  }
+
+  const file = req.files.file;
+
+  // Make sure the image is a photo
+  if (!file.mimetype.startsWith('image')) {
+    return next(new ErrorResponse(`Please upload a image file`, 400));
+  }
+
+  // Check filesize
+  if (file.size > process.env.MAX_FILE_UPLOAD) {
+    return next(
+      new ErrorResponse(
+        `Please upload a image less than ${process.env.MAX_FILE_UPLOAD}`,
+        400
+      )
+    );
+  }
+
+  //  Create custom filename
+  file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
+
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
+    if (err) {
+      console.log(err);
+      return next(new ErrorResponse(`Problem with file upload`, 500));
+    }
+    await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
+
+    res.status(200).json({
+      success: true,
+      data: file.name,
+    });
+  });
+
+  console.log(file.name);
 });
--- seeder.js
+++ seeder.js
@@ -30,7 +30,7 @@ const courses = JSON.parse(
 const imporData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-    //await Course.create(courses);
+    await Course.create(courses);
 
     console.log('Data Imported...'.green.inverse);
     process.exit();
